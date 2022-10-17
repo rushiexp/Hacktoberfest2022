7f9e476a8a33bdc2f2c54485fd589fe8afd1d1b2
rushiexp <116000298+rushiexp@users.noreply.github.com>
2022-10-17 18:38:22 +0530

    Add files via upload

---
+++ contributors/longsubstring.java
@@ -0,0 +1,50 @@
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+
+public static Boolean areDistinct(String str,
+								int i, int j)
+{
+	
+	boolean[] visited = new boolean[26];
+
+	for(int k = i; k <= j; k++)
+	{
+		if (visited[str.charAt(k) - 'a'] == true)
+			return false;
+			
+		visited[str.charAt(k) - 'a'] = true;
+	}
+	return true;
+}
+
+public static int longestUniqueSubsttr(String str)
+{
+	int n = str.length();
+	
+	// Result
+	int res = 0;
+	
+	for(int i = 0; i < n; i++)
+		for(int j = i; j < n; j++)
+			if (areDistinct(str, i, j))
+				res = Math.max(res, j - i + 1);
+				
+	return res;
+}
+
+
+public static void main(String[] args)
+{
+	String str = "bbbbb";
+	System.out.println("The input string is " + str);
+
+	int len = longestUniqueSubsttr(str);
+	
+	System.out.println("The length of the longest " +
+					"non-repeating character " +
+					"substring is " + len + " which is b");
+}
+}
